1ec2746c3b726088c77a4d4e143ae34bd6927b60
OceanGajbhiye <57103064+OceanGajbhiye@users.noreply.github.com>
2022-12-18 22:28:52 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8bd29b8b71msh008cfcb0cdce7e1p13346fjsne6e84f8abbdb",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d47123539b78c7934d6ec7cd6c1b2df0"; 
